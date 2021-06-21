a005009f23bc07e8e8b08ccd5a2e9e0405b63b13
David Molinari <david.m.molinari@gmail.com>
2021-06-21 17:36:40 -0400

    v2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React, { useEffect, useState } from "react";
 import './App.css';
-import axios from "axios";
 import moment from "moment";
 import Nav from "./components/Nav";
 import Graph from "./components/Graph";
+import {
+  useQuery,
+  gql
+} from "@apollo/client";
+
+const GET_RANGES_AND_DATA = gql`
+  query GetRangesAndData {
+    ranges {
+      low_bound
+      high_bound
+      color
+    }
+    data {
+      result_id
+      result_dt_tm
+      glucose_level
+      glucose_level_unit
+      source
+    }
+  }
+`;
 
 function App() {
 
@@ -14,35 +34,42 @@ function App() {
     end: ""
   })
 
-  // Fetch glucose data on app mount, and set initial state
+  const { loading, error, data } = useQuery(GET_RANGES_AND_DATA);
+
+  // On load of data, set initial state
   useEffect(()=> {
-    axios.get(`https://caiken.dev.transformativemed.com/david-skills-test/data.php`)
-      .then(res => {
-        let data = res.data
-        setGlucoseData(data.glucose_data)
-        setGlucoseRanges(data.glucose_ranges)
-        let startDT = data.glucose_data[0].result_dt_tm
-        let startD = moment(startDT.slice(0, startDT.search(" "))).format()
-        let endDT = data.glucose_data[data.glucose_data.length - 1].result_dt_tm
-        let endD = moment(endDT.slice(0, endDT.search(" "))).format()
-        setSelectedDates({
-          start: startD,
-          end: endD
-        })
+    if (!loading) {
+      setGlucoseData(data.data)
+      let rangesToArrays = []
+      data.ranges.forEach((r)=> {
+        rangesToArrays.push([r.low_bound, r.high_bound, r.color])
       })
-  }, [])
+      setGlucoseRanges(rangesToArrays)
+      let startDT = data.data[0].result_dt_tm
+      let startD = moment(startDT.slice(0, startDT.search(" "))).format()
+      let endDT = data.data[data.data.length - 1].result_dt_tm
+      let endD = moment(endDT.slice(0, endDT.search(" "))).format()
+      setSelectedDates({
+        start: startD,
+        end: endD
+      })
+    }
+  }, [data])
+
+  if (error) return `${error}`;
 
   return (
     <>
-      {glucoseData.length ? 
+      {!loading && glucoseData.length ? 
         <div className="App">
           <Nav selectedDates={selectedDates} setSelectedDates={setSelectedDates} glucoseData={glucoseData}/>
           <Graph selectedDates={selectedDates} glucoseData={glucoseData} glucoseRanges={glucoseRanges}/>
         </div>
       : 
-      <h1 id="Loading">
-        Loading...
-      </h1>}
+        <h1 id="Loading">
+          Loading...
+        </h1>
+      }
     </>
   );
 }
